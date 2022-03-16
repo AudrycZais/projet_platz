a3392ea77930f5ebab6e80f5ea5d67fbbb5e858b
Zais Audryc <audryczais@gmail.com>
2022-03-16 21:17:59 +0100

    params + returns

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
+    /**
+     * Ajout d'un commentaire
+     *
+     * @param Request $request
+     * @return Factory
+     */
     public function addComment(Request $request){
         $comment = new Comment;
         $comment->pseudo = $request->pseudo;
--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,20 +7,40 @@ use App\Models\Post;
 
 class PostsController extends Controller
 {
+    /**
+     * Index des posts
+     *
+     * @return Factory
+     */
     public function index(){
         $posts=Post::orderby('created_at', 'desc')->take(20)->get();
         return view('post.index' , compact('posts'));
     }
-
+    /**
+     * Le détail d'un post
+     *
+     * @param Post $post
+     * @return Factory
+     */
     public function show(Post $post){
         return view('post.show', compact('post'));
     }
-
+    /**
+     * L'index des catégories
+     *
+     * @param integer $category_id
+     * @return Factory
+     */
     public function ajaxCategory(int $category_id){
         $posts=Post::where('category_id', '=', $category_id)->orderby('created_at', 'desc')->take(20)->get();
         return view('post._list' , compact('posts'));
     }
-
+/**
+ * Chargement de plus de posts (10)
+ *
+ * @param Request $request
+ * @return Factory
+ */
     public function more(Request $request){
         $posts=Post::orderby('created_at', 'desc')->take(10)->offset($request->offSet)->get();
         return view('post._list' , compact('posts'));
